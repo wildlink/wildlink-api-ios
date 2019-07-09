644574b1e64488304c8910ca423a305a895d58b6
Kyle Kurz <kyle@doublekaudio.com>
2019-07-08 20:48:38 -0700

    Better logs on example calls

--- Example/Wildlink/ViewController.swift
+++ Example/Wildlink/ViewController.swift
@@ -25,20 +25,20 @@ class ViewController: UIViewController {
                 self.urlOutlet.text = url.absoluteString
                 sleep(2)
                 Wildlink.shared.getClickStats(from: Date(timeIntervalSinceNow: -604800), with: .hour, completion: { (results, error) in
-                    print("Click stats: \(String(describing: error))")
-                    print("Click stats: \(String(describing: results))")
+                    print("Click stats error: \(String(describing: error))")
+                    print("Click stats results: \(String(describing: results))")
                 })
                 Wildlink.shared.getCommissionSummary({ (stats, error) in
-                    print("Click stats: \(String(describing: error))")
-                    print("Commission summary: \(String(describing: stats))")
+                    print("Commission summary error: \(String(describing: error))")
+                    print("Commission summary results: \(String(describing: stats))")
                 })
                 Wildlink.shared.getMerchantByID("5476062", { (merchant, error) in
-                    print("Click stats: \(String(describing: error))")
-                    print("Merchant data: \(String(describing: merchant))")
+                    print("Merchant data error: \(String(describing: error))")
+                    print("Merchant data results: \(String(describing: merchant))")
                 })
                 Wildlink.shared.searchMerchants(ids: [], names: [], q: nil, disabled: nil, featured: true, sortBy: nil, sortOrder: nil, limit: nil, { (merchants, error) in
-                    print("Click stats: \(String(describing: error))")
-                    print("List of merchants: \(merchants)")
+                    print("List of merchants error: \(String(describing: error))")
+                    print("List of merchants results: \(merchants)")
                 })
             }
         }
