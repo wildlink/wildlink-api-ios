e70564c815b16ee799af41269559fb8375857ced
Kyle Kurz <kyle@doublekaudio.com>
2019-07-08 20:02:23 -0700

    Add test target

--- Example/Wildlink_ExampleTests/Wildlink_ExampleTests.swift
+++ Example/Wildlink_ExampleTests/Wildlink_ExampleTests.swift
@@ -2,7 +2,7 @@
 //  Wildlink_ExampleTests.swift
 //  Wildlink_ExampleTests
 //
-//  Created by Kyle Kurz on 6/30/19.
+//  Created by Kyle Kurz on 7/8/19.
 //  Copyright © 2019 CocoaPods. All rights reserved.
 //
 
@@ -17,4 +17,17 @@ class Wildlink_ExampleTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
 }
